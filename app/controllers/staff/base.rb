9c43512bdbcf2f0b637fa7f4e88e1a0740a6775b
Osadakun <b1915053@planet.kanazawa-it.ac.jp>
2021-09-15 10:28:52 +0000

    add file

--- app/controllers/staff/base.rb
+++ app/controllers/staff/base.rb
@@ -1,5 +1,5 @@
 class Staff::Base < ApplicationController
-    private defcurrent_staff_member
+    private def current_staff_member
         if session[:staff_member_id]
            @current_staff_member ||=
                 StaffMember.find_by(id:session[:staff_member_id])
